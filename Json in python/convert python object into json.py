a5ec93b522a0f558e9b52d4f5bed3682bd49f712
pc <fahadbinwadud.iiuc.cse@gmail.com>
2024-05-18 22:48:49 +0600

    polymorphism,Scope,Modules,Dates and Json exercise done

---
+++ Json in python/convert python object into json.py
@@ -0,0 +1,9 @@
+import  json
+print(json.dumps({"name":"Fahad","age":28,"gender":"Male"}))
+print(json.dumps(["apple","Orange"]))
+print(json.dumps(("jackfruit","Coconut")))
+print(json.dumps("Hello"))
+print(json.dumps(31.24))
+print(json.dumps(True))
+print(json.dumps(False))
+print(json.dumps(None))
